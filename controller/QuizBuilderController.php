316280a007cc2cee9a48f6552aa1dc04dfb6db3b
prottoy94 <prottoyroy900@gmail.com>
2026-05-12 23:32:45 +0600

    Add QuizBuilderController and related files for quiz management

---
+++ controller/QuizBuilderController.php
@@ -0,0 +1,27 @@
+<?php
+
+//declare(strict_types=1);
+
+// include "../model/db.php";
+
+class QuizBuilderController
+{
+    public function getQuizListData(): array
+    {
+        $quizzes = array();
+        $stats = array(
+            'total_quizzes' => 0,
+            'published_quizzes' => 0,
+            'draft_quizzes' => 0,
+            'total_attempts' => 0,
+            'average_score' => 0,
+        );
+
+        return array(
+            'quizzes' => $quizzes,
+            'stats' => $stats,
+            'page_title' => 'My Quizzes',
+            'page_subtitle' => 'Manage, publish, and monitor your quiz library',
+        );
+    }
+}

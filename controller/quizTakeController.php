aaa0eab9088237897719fa1397f63c0ce1d74771
Salman Sadi <salmansadi1010@gmail.com>
2026-05-14 19:49:34 +0600

    fetching data from db to pass to the page

--- controller/quizTakeController.php
+++ controller/quizTakeController.php
@@ -6,6 +6,28 @@ if (session_status() === PHP_SESSION_NONE)
 
 include __DIR__ . "/../model/quizTakeModel.php";
 
+$attemptId= "";
+$quizId = "";
+$totalMarks = "";
+$score = "";
+$title = "";
 
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $attemptId = intval($_GET['attempt']);
+
+    $db = new quizTakeDb();
+    $quiz = $db->get_quiz_info($attemptId);
+    $quizId = $quiz['quizId'];
+    $questions = $db->get_quiz_questions($quizId);
+    $quizData = [
+        "quizTitle" => $quiz['title'],
+        "instructor"=> $quiz['instructor'],
+        "totalMarks"=> $quiz['totalMarks'],
+        "questions" => $questions
+    ];
+
+    header('Content-Type: application/json');
+    echo json_encode($quizData);
+}
 
 ?>

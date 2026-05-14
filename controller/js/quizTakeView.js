f416d596cff9a679e7a8b9093d8571ad70a76d63
Salman Sadi <salmansadi1010@gmail.com>
2026-05-15 02:49:45 +0600

    Sadi (#54)
    * Added quiz listing
    
    Displayed only published quizzes. Greyed out attempted quizzes with score shown. Added "Start Quiz" button for unattempted quizzes with time limit.
    
    * added redirection to takequiz page and db, php js validations related to this feature
    
    * added the required db functions for quiz taking and grading
    
    * updated the get quiz info function
    
    * fetching data from db to pass to the page
    
    * minor changes to the quizinfo function
    
    * redefined some functions to fetch db data
    
    * added the quiz view feature, the quiz info is loading into a input field of radio buttons
    
    * added the take quiz logic, grading, etc

--- controller/js/quizTakeView.js
+++ controller/js/quizTakeView.js
@@ -23,5 +23,48 @@ document.addEventListener("DOMContentLoaded", function() {
                 container.innerHTML += html;
             });
         })
-        .catch(err => alert("Error loading quiz:", err));
+    .catch(err => alert("Error loading quiz:", err));
+
+    
+
+    const form = document.getElementById("quiz-form");
+    if (!form) {
+        console.error("quiz-form element not found!");
+        return;
+    }
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        const answers = {};
+        document.querySelectorAll("input[type=radio]:checked").forEach(input => {
+            const qid = input.name.replace("q", "");
+            answers[qid] = input.value;
+        });
+
+        const payload = {
+            attempt_id: attemptId,
+            answers: answers
+        };
+
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "../../controller/quizTakeController.php", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    const response = JSON.parse(xhr.responseText);
+                    console.log("Submit response:", response);
+                    //window.location.href = `/view/student/quiz_result.php`;
+                } else {
+                    console.error("Error submitting quiz:", xhr.responseText);
+                    alert("Submission failed.");
+                }
+            }
+        };
+
+        xhr.send(JSON.stringify(payload));
+    });
+
 });
+

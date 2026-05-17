c531d568be7f5d2f892d4f1b13f66f37683ec300
Salman Sadi <salmansadi1010@gmail.com>
2026-05-17 18:38:14 +0600

    added the back button functionalities and minor changes to each files

--- view/student/quiz_list.php
+++ view/student/quiz_list.php
@@ -14,16 +14,11 @@ if(session_status() === PHP_SESSION_NONE) {
   <h2>Available Quizzes</h2>
   <p id="subh">Select a quiz to start. Attempted quizzes are shown with your score.</p>
 
-  <form method="post" action="">
-    <div id="quiz-list">
-        <p id="noQuizzes" name="noQuizzes"></p>
-    </div>
+  <div id="quiz-list">
+      <p id="noQuizzes" name="noQuizzes"></p>
+  </div>
 
-    <form method="POST" action="">
-        <input type="hidden" name="goDashboard" value="1">
-        <button id="back" name="back" type="submit">Back</button>
-    </form>
-  </form>
+  <button id="back" name="back" type="button" onclick="window.location.href='dashboard.php'">Back</button>
   <script src="../../controller/js/quizListView.js" defer></script>
 </body>
 </html>
